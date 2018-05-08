f32647333923b0f826ec53d1d4ac3be130da0ce0
Cristina Lugo <cmlugoce@syr.edu>
2018-05-08 21:45:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,7 @@ function addElementToBeginningOfArray (array, element) {
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
 return array.unshift(element)
+return array
 }
 
 function addElementToEndOfArray (array, element) {
